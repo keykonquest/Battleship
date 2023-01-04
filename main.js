913990ae934fc1e47c92acb379dec3a7bab81d0e
Key <kpillorsrocks@gmail.com>
2023-01-04 00:54:49 -0500

    Add methods to objects
    Complete fire mthod. Add isSunk and processGuess methods to game
    
    [Ticket: Z00005]

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 4 
+// 5 
 
 let shipSunk = false;
 
@@ -25,22 +25,65 @@ let gameModel = {
     numShips: 2,
     shipsSunk: 0,
     numGuesses: 0,
-    ships: [{locations: ['10', '20', '30'], hits: ["", "", ""] },
-            {locations: ['43', '44', '45'], hits: ["", "", ""] }],
+    ships: [{locations: ['10', '20', '30'], hits: ["", "", ""]},
+            {locations: ['43', '44', '45'], hits: ["", "", ""]},
+            {locations: ['10', '20', '30'], hits: ["", "", ""]}],
     fire: function(guess) {
-        for (i = 0; i < this.numShips; i++) {
+        for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
-            if (guess in ship["locations"]) {
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "x";
                 display.displayHit(guess);
-            } 
+                display.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    display.displayMessage("You've sunken my battleship!");
+                    this.shipsSunk++;
+                }
+                return;
+            } else {
+                display.displayMessage("MISS!")
+                display.displayMiss(guess);
+            }
         }
+    },
+    isSunk: function(ship){
+        for (let i = 0; i < 3; i++) {
+            if (ship.hits[i] !== 'x') {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
 
 let brain = {
+
+    processGuess: function(guess) {
+        let location = this.processGuess(guess);
+        if (location) {
+            let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+            if (guess == null || guess.length !== 2) {
+                display.displayMessage("Invalid coordinate")
+            };
+            row = alphabet.indexOf(guess.charAt(0));
+            column = guess.charAt(1);
+            if (isNaN(row) || isNaN(column)) {
+                alert("That is not a valid input");
+            } else if (row < 0 || row > gameModel.boardsize || column < 0 || column > gameModel.boardsize) {
+                alert("That is not a valid input");
+            } else {
+                return row + column;
+                gameModel.numGuesses++
+            }
+            return null;
+        }
+        
+    },
+
     createShip: function() {
-        let bow = Math.floor(Math.random() *  );
+        let bow = Math.floor(Math.random() * 5 );
     },
     addSegment: function(head) {
         let direction = Math.random();
@@ -51,11 +94,11 @@ let brain = {
             }
         } else {
             this.segment = head + 10;
-            if
-        }
+        };
     }
 };
 
 
-let stats = "It took you " + guesses + " shots to sink the ship.\n" + "Accuracy: " + (3/guesses);
-alert(stats);
+
+//let stats = "It took you " + guesses + " shots to sink the ship.\n" + "Accuracy: " + (3/guesses);
+//alert(stats);
